8d384bb41539dbecfa3c2ae175f747981a55d594
sadhik shaik <140734180+sadhikdev@users.noreply.github.com>
2024-04-23 19:07:17 +0530

    Add files via upload

---
+++ cdn.jsdelivr.net/sm/jsdelivr-header.js
@@ -0,0 +1,6 @@
+/**
+ * Minified by jsDelivr using Terser v5.19.2.
+ * Original file: /npm/canvas-confetti@1.9.0/dist/confetti.browser.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
